e8de5e266c6dfd92c71e2406c5fd0fae50af0b15
William Wang <javaornettutor@gmail.com>
2025-01-30 10:14:23 +1100

    commit to live

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+import HelloWorld from './Simple/HelloWorld'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+<HelloWorld />
   </React.StrictMode>
 );
 
